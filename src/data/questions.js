cf21c736b4bf7cac8c641d2847443ff46c0f242f
Alexandro Milhomes Leão <alexmilhomes.aml@gmail.com>
2023-02-20 09:35:36 -0300

    Primeiro Commit

---
+++ src/data/questions.js
@@ -0,0 +1,65 @@
+const data = [
+    {
+      question: "Qual o nome do pai de Marceline?",
+      options: [
+        "Rei Gelado",
+        "Hanson Abeder",
+        "Hunson Abadeer",
+        "Hanson Ubeder",
+      ],
+      answer: "Hunson Abadeer",
+    },
+    {
+      question: "Qual personagem é capaz de conceder quase qualquer desejo?",
+      options: ["Prismo", "Coruja Cósmica", "Golb", "Grob Gob Glob Grod"],
+      answer: "Prismo",
+    },
+    {
+      question: "Qual desses NÂO é um dos filho de Jake?",
+      options: ["Charlie", "Kim-Kil-Whan", "T.V", "Blimbo"],
+      answer: "Blimbo",
+    },
+    {
+      question: "Quem criou o BMO?",
+      options: ["Moseph Maestro Giovanni", "Moe Maestro Giovanny", "Moseph Mastro Giovannine", "Moseph Mastro Giovanni"],
+      answer: "Moseph Mastro Giovanni",
+    },
+    {
+      question:
+        "Qual desses personagens foi realmente uma das encarnações de Finn?",
+      options: ["Joshua", "Tiffany", "Shoko", "Cuber"],
+      answer: "Shoko",
+    },
+    {
+      question:
+        "Qual o verdadeiro nome do Rei Gelado?",
+      options: ["Martin", "Simon", "Lenny", "Peter"],
+      answer: "Simon",
+    },
+    {
+      question:
+        "Qual o nome do robô que Finn cria para jogar tortas em Jake?",
+      options: ["Moe", "Ronnie", "CMO", "Neptr"],
+      answer: "Neptr",
+    },
+    {
+      question:
+        "Gunter é na verdade uma entidade cósmica, e seu verdadeiro nome é:",
+      options: ["Evergreen", "Orgalorg", "Lich", "Glob"],
+      answer: "Orgalorg",
+    },
+    {
+      question:
+        "Qual personagem se tornou a nova Morte?",
+      options: ["Sr.Raposo", "Marceline", "Bartram", "Shelby"],
+      answer: "Sr.Raposo",
+    },
+    {
+      question:
+        "Qual personagem foi criado a partir do DNA de Finn para deter Goliad?",
+      options: ["Faísca", "Fern", "Stormo", "Giuseppe"],
+      answer: "Stormo",
+    },
+  ];
+  
+  export default data;
